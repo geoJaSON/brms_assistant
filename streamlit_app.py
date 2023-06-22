00bd7649166239963b6d0d7462ad68fe10211e43
Jason Jordan <47825739+geoJaSON@users.noreply.github.com>
2023-06-22 07:11:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,10 @@ css = """
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
-    background-color: #2b313e
+    background-color: #94e5ff
 }
 .chat-message.bot {
-    background-color: #475063
+    background-color: #FF7276
 }
 .chat-message .avatar {
   width: 20%;
