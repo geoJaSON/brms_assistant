4aab3e20002f309c2499bc6acc91049c9a18e4dc
Jason Jordan <47825739+geoJaSON@users.noreply.github.com>
2024-03-07 10:12:36 -0600

    Update streamlit_app.py
    Rewite with Chroma

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,37 +1,58 @@
 #%%
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
-import pinecone
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 import streamlit as st
-#%%
-OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
-PINECONE_API_KEY = st.secrets["PINECONE_API_KEY"]
-PINECONE_API_ENV = 'northamerica-northeast1-gcp'
+
+
+#%%pip install chroma
+persist_directory = 'db'
+embedding = OpenAIEmbeddings(api_key=st.secrets["OPENAI_API_KEY"])
+
+vectordb2 = Chroma(persist_directory=persist_directory, 
+                  embedding_function=embedding,
+                   )
+
+retriever = vectordb2.as_retriever(search_kwargs={"k": 2})
 
 #%%
-pinecone.init(
-    api_key=PINECONE_API_KEY,  # find at app.pinecone.io
-    environment=PINECONE_API_ENV  # next to api key in console
+# Set up the turbo LLM
+turbo_llm = ChatOpenAI(
+    temperature=0,
+    model_name='gpt-3.5-turbo'
 )
+# create the chain to answer questions 
+qa_chain = RetrievalQA.from_chain_type(llm=turbo_llm, 
+                                  chain_type="stuff", 
+                                  retriever=retriever, 
+                                  return_source_documents=True)
+#%%#%%
 
-embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
-llm = OpenAI(temperature=0, openai_api_key=OPENAI_API_KEY)
-chain = load_qa_chain(llm, chain_type="stuff")
-docsearch = Pinecone.from_existing_index('esfsearch', embeddings)
+def ask_question(question):
+    try:
+        llm_response = qa_chain(question)
+        try:
+            source = llm_response["source_documents"][0].metadata['source'].replace(r"C:\Users\g3retjjj\Documents\chatbot_data\sop.txt",r"\\nwk-netapp2.nwk.ds.usace.army.mil\MISSIONFILES\MissionProjects\civ\Temporary Roofing\2.0 SOP\2023\2023 Temporary Roofing SOP.pdf")
+            return llm_response['result'] + '\n\nSource: ' + source
+        except:
+            return "Apologies but I cannot find the answer in my source material"
+        
+
+    except Exception as e:
+        st.error(e)
+
 #%%
-
 css = """
 <style>
 .chat-message {
     padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
 }
 .chat-message.user {
-    background-color: #00008B
+    background-color: #94e5ff
 }
 .chat-message.bot {
-    background-color: #8B0000
+    background-color: #FF7276
 }
 .chat-message .avatar {
   width: 20%;
@@ -69,12 +90,6 @@ user_template = """
 st.markdown(css, unsafe_allow_html=True)
 
 
-def ask_question(question):
-    try:
-        docs = docsearch.similarity_search(question)
-        return chain.run(input_documents=docs, question=question)
-    except Exception as e:
-        st.error("An error has occurred. Please try again.")
 
 # Initialize session state if not yet done
 if "chat_history" not in st.session_state:
